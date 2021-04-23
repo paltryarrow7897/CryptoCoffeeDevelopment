24bc871da5e31c3f87fa96c2f2384bc95619c8f4
Kushagra Jain <67114340+paltryarrow7897@users.noreply.github.com>
2021-04-23 08:38:36 +0530

    Update CryptoCoffee.sol

--- CryptoCoffee.sol
+++ CryptoCoffee.sol
@@ -47,20 +47,26 @@ contract CryptoCoffee is ERC721URIStorage {
   }
   
   function setPricePutOnSale(uint _tokenId, uint128 _amount) public {
+  
+  // we want the seller to set a price on his NFT and put it on sale.
+  // if he doesn't set a price, the NFT is not sellable.
+  // right now, as soon as seller sets price, NFT is added to sale.
+  // it can changed by giving seller an option to put on sale right now or schedule for later.
+  
     require(_exists(_tokenId));
     require(_isApprovedOrOwner(msg.sender, _tokenId));
     
 //    tokenIdToPrice[_tokenId] = _amount;
 
     Sale storage sale = tokenIdToSale[_tokenId];
-    sale.seller = payable(msg.sender);
-    sale.price = _amount;
-    sale.onSale = true;
+    sale.seller = payable(msg.sender); // seller address
+    sale.price = _amount; // seller sets NFT price
+    sale.onSale = true; // seller turns on put on sale 
     
     emit nftPriceSet(_tokenId, _amount);
   }
   
-/*
+/* Not used anymore.
   function sell_NFT(address _owner, address _buyer, uint256 _tokenId) public {
     require(_exists(_tokenId));
     require(_isApprovedOrOwner(_owner, _tokenId));
@@ -69,6 +75,11 @@ contract CryptoCoffee is ERC721URIStorage {
 */
 
   function transfer_NFT(address _buyer, uint _tokenId) public {
+  
+  // this needs editing.
+  // this function should be called by the seller only.
+  // seller transfers NFT after confirming payment.
+  
     require(_buyer != address(0));
     require(msg.sender != address(0));
     require(_exists(_tokenId));
@@ -81,12 +92,24 @@ contract CryptoCoffee is ERC721URIStorage {
   }
 
   function burn_NFT(address _owner, uint256 _tokenId) public {
+  
+  // this burns NFT but doesn't give back ingredients.
+  // I haven't given much time to it yet.
+  // only owner can burn his NFT.
+  
       require(_exists(_tokenId));
       require(_isApprovedOrOwner(_owner, _tokenId));
       _burn(_tokenId);
   }
   
   function buyAtSale(uint256 _tokenId, uint256 _price) public payable {
+  
+  // I tried copying cryptokitties auction contract.
+  // what is should do is allow buyer to transfer ether to seller's
+  // address. if buyer's price is more than seller's price, refund 
+  // is issued and the sale for that NFT ends so that no other buyer
+  // can accidentally pay for the sold NFT.
+  
       Sale storage sale = tokenIdToSale[_tokenId];
       require(sale.onSale);
       require(_price >= sale.price);
@@ -108,7 +131,11 @@ contract CryptoCoffee is ERC721URIStorage {
   }  
 
 //  function penalty_on_NFT(NFT memory nft) public {}
-/*
+// penalty on NFT not implemented.
+
+/* user_NFTs not done yet. there is a function in cryptokitties which
+// does the same thing. I was trying to copy and modify that.
+
   function user_NFTs(address _owner) external view returns (NFT[]) {
       
     userBalance = balanceOf(_owner);
