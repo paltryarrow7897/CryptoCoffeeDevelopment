5db5692e85276e73954394efc04c4fdf6aaa370f
Kushagra Jain <67114340+paltryarrow7897@users.noreply.github.com>
2021-05-07 21:20:30 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,3 +1,6 @@
+/* ERC20Preset. **********************************
+** Creates ERC20 token supply for all projects. */
+
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol';
